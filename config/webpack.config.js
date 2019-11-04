4f5f59a10d24edf092a172fbbff493fb674952ab
RickButler <rick@rickbutler.com>
2019-11-04 15:46:08 -0500

    fix eslint

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -373,7 +373,7 @@ module.exports = function(webpackEnv) {
                     return eslintConfig;
                   } else {
                     return {
-                      extends: [require.resolve('eslint-config-react-app')],
+                      extends: [require.resolve('eslint-config-airbnb-base')],
                     };
                   }
                 })(),
