1f8bb0f1fab96856e49e1c9915d03331769af80b
RickButler <rick@rickbutler.com>
2019-11-21 18:42:46 -0500

    add html loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -570,6 +570,10 @@ module.exports = function (webpackEnv) {
                     path.join(paths.appSrc, 'templates', 'partials')
                 ]
               }
+            },                       
+            {
+              loader: require.resolve('html-loader'),
+              test: /\.html$/
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
